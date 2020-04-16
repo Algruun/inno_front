3b7b15ed0561bdacac17b4597f6156d75d5265be
A <algruun@MacMiniW.lan>
2020-04-16 05:00:45 +0300

    update button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,14 +17,14 @@ class NavBar extends Component{
       <Menu fixed='top' borderless id='navbar'>
         <Container>
           <Menu.Item as={Link} to='/menu' id='navbar-header'>Pizza task</Menu.Item>
-          <Menu.Item id='navbar-currency' position='right'>
-            <Icon name={this.currency}/>
-            <Button onClick={() => this.props.changeCurrency()} color='teal'>Add to order</Button>
-          </Menu.Item>
           <Menu.Item id='navbar-total' position='right'>
             <Icon name='cart'/>
             {formatPrice(this.props.orderTotal, this.props.currency)}
             {this.props.children}
+            <br/>
+            <Button id={'menu-checkout-btn'} className='ui icon button' onClick={() => this.props.changeCurrency()} color='teal'>
+              <Icon name={(this.props.currency === 'USD') ? 'euro': 'dollar'}/>
+            </Button>
           </Menu.Item>
         </Container>
       </Menu>
